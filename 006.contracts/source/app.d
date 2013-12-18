ed8cfe7605c6a2f28638fc175081cf1da12a1667
Mason Browne <mason.browne@gmail.com>
2013-12-17 21:36:17 -0800

    enforce.

--- 006.contracts/source/app.d
+++ 006.contracts/source/app.d
@@ -1,6 +1,6 @@
 import std.stdio;
 import std.random;
-
+import std.exception;
 
 int test1(int x)
 in {
@@ -13,9 +13,7 @@ body {
     return x + 1;
 }
 
-unittest {}
-
-void main() {
+unittest {
     test1(1);
     bool caught = false;
     try {
@@ -26,5 +24,49 @@ void main() {
     } finally {
         assert(caught);
     }
+}
 
+class Test {
+    int one, five, ten;
+    invariant() {
+        assert(one >= 1);
+        assert(five >= 5);
+        assert(ten >= 10);
+    }
+    this() {
+        one = 1;
+        five = 5;
+        ten = 10;
+    }
+
+    void testOne(int v) {
+        one = v;
+    }
+
+}
+
+unittest {
+    auto t = new Test;
+    bool caught = false;
+    try {
+        t.testOne(-1);
+    } catch(core.exception.AssertError e) {
+        assert(e);
+        caught = true;
+    } finally {
+        assert(caught);
+    }
+}
+
+void main() {
+    // blatantly abusing `enforce' so I can test in release
+    auto caught = false;
+    try {
+        enforce(0);
+    } catch(Exception e) {
+        enforce(e);
+        caught = true;
+    } finally {
+        enforce(caught);
+    }
 }
