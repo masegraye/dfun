f5953a45a1e0d544b483d3cfab05c6c4561e4032
Mason Browne <mason.browne@gmail.com>
2013-12-18 00:25:13 -0800

    Playing with opDispatch - dynamic method registration/invocation

---
+++ 007.operator_overloading/source/app.d
@@ -0,0 +1,87 @@
+import std.stdio;
+import std.variant;
+
+class A {
+    int opApply(int delegate(int) dg) {
+        for (int i = 0; i <= 3; i++) {
+            dg(i);
+        }
+        return 0;
+    }
+}
+
+unittest {
+    auto a = new A;
+    auto sum = 0;
+    foreach(i; a) {
+        sum += i;
+    }
+    assert(6 == sum);
+}
+
+class B {
+    int testOne() {
+        return 1;
+    }
+
+    auto opDispatch(string m, Args...)(Args args) {
+        if (m == "testTwo") {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+}
+
+unittest {
+    auto b = new B;
+    assert(1 == b.testOne());
+    assert(2 == b.testTwo());
+    assert(3 == b.testThree());
+    assert(3 == b.testFour());
+}
+
+alias Variant delegate(Dynamic self, Variant[] args) DynMethod;
+
+class Dynamic {
+    private DynMethod[string] methods;
+
+    void addMethod(string name, DynMethod m) {
+        methods[name] = m;
+    }
+
+    void removeMethod(string name) {
+        methods.remove(name);
+    }
+
+    Variant call(string methodName, Variant[] args) {
+        return methods[methodName](this, args);
+    }
+
+    Variant opDispatch(string m, Args...)(Args args) {
+        Variant[] packedArgs = new Variant[args.length];
+
+        foreach(i, arg; args) {
+            packedArgs[i] = Variant(arg);
+        }
+
+        return call(m, packedArgs);
+    }
+
+}
+
+unittest {
+    auto obj = new Dynamic;
+
+    obj.addMethod("testOne",
+        (Dynamic, Variant[]) {
+            writeln("testOne");
+            return Variant();
+    });
+
+    obj.testOne();
+
+
+}
+
+void main() {}
