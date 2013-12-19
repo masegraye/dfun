1c351cb98198baed8241584cff26eae743b62113
Mason Browne <mason.browne@gmail.com>
2013-12-18 22:18:20 -0800

    Tabs. Ew.

--- 009.concurrency_2/source/app.d
+++ 009.concurrency_2/source/app.d
@@ -2,7 +2,7 @@ import std.algorithm, std.concurrency, std.stdio;
 
 void main()
 {
-	enum bufferSize = 1024 * 100;
+    enum bufferSize = 1024 * 100;
     auto tid = spawn(&fileWriter);
     foreach (buffer; stdin.byChunk(bufferSize)) {
         send(tid, buffer.idup);
