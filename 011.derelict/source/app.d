dddab19a3bc96286be876f4d1352bacf1d5177a1
Mason Browne <mason.browne@gmail.com>
2013-12-29 16:21:24 -0800

    Magic triangle.

--- 011.derelict/source/app.d
+++ 011.derelict/source/app.d
@@ -1,10 +1,10 @@
 import std.stdio;
-import derelict.opengl3.gl3;
+import derelict.opengl3.gl;
 import derelict.glfw3.glfw3;
 
 void main()
 {
-	DerelictGL3.load();
+	DerelictGL.load();
     DerelictGLFW3.load();
 
     if (!glfwInit()) throw new Exception("Failed to initialize GLFW");
@@ -12,10 +12,37 @@ void main()
     GLFWwindow* window = glfwCreateWindow(800, 600, "AWW YEAH 011.derelict", null, null);
     if (!window) throw new Exception("Uh oh. No window.");
 
+
+    glfwMakeContextCurrent(window);
+
+
     while (!glfwWindowShouldClose(window))
     {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / cast(float)height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef(cast(float)glfwGetTime() * 50.0f, 0.0f, 0.0f, 1.0f);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-0.6f, -0.4f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.6f, -0.4f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.6f, 0.0f);
+        glEnd();
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
+
 }
